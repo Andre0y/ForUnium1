f802b782e743b6e8e984cc08712d0e16942e34b9
Andre0y <99538567+Andre0y@users.noreply.github.com>
2022-09-18 21:41:25 +0300

    New

---
+++ MyProjectForUnium1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyProjectForUnium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPeopleInQueue = 100;
+            int timeOfWaiting = 10;
+            int positionInQueue = 95;
+            int totalTimeOfWaiting = timeOfWaiting * positionInQueue;
+
+            Console.WriteLine("Количество человек в очереди: " + totalPeopleInQueue + "," + " Время ожидания: " + timeOfWaiting + " Секунд, " + " Позиция в очереди: " + positionInQueue + "-й, " + "Время ожидания в очереди: " + totalTimeOfWaiting + " Секунд");
+        }
+    }
+}
